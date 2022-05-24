c96a8d3bb908be921c79f40051ab477eab0e1e28
doyeon <doyeon311@gmail.com>
2022-05-24 23:55:43 +0900

    #2.2 Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 function App() {
   return (
     <Father>
-      <BoxOne>
+      <Box bgColor="teal">
         <Text>
           Hello
         </Text>
-      </BoxOne>
-      <BoxTwo />
+      </Box>
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
@@ -19,18 +19,16 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${props => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
 const Text = styled.span`
   color: white;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
