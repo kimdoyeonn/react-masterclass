4cafaa0c599f43505073a9744d4e9c0d0df49df5
doyeon <doyeon311@gmail.com>
2022-06-01 23:41:08 +0900

    #4.0 Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import Router from "./Router";
 
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const { currentTarget: { value }} = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Hello", value);
-  };
-
   return (
-    <Container>
-      <H1>Hello</H1>
-    </Container>
+    <Router />
   );
 }
 
 export default App;
-
-const Container = styled.div`
-  background-color: ${props => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${props => props.theme.textColor};
-`;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-import { darkTheme, lightTheme } from './theme';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
