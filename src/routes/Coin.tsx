5d16b7397211c3d67bc9a84ded5459a5a4c81560
doyeon <doyeon311@gmail.com>
2022-06-08 22:35:39 +0900

    #4.5 Coin Data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
-import { Container, Header, Title } from "./Coints";
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { Container, Header, Title } from './Coints';
 
 interface RouteParams {
   coinId: string;
@@ -16,16 +16,26 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
   const { state } = useLocation() as RouterLocation;
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+      const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading..."}</Title>
+        <Title>{state?.name || 'Loading...'}</Title>
       </Header>
-      {loading ? "loading..." : null}
+      {loading ? 'loading...' : null}
     </Container>
   );
 }
 
 export default Coin;
-
