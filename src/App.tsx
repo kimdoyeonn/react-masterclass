a1b8322f6ca7285bac0ef5dc9169e4a7039abc63
doyeon <doyeon311@gmail.com>
2022-06-21 23:48:31 +0900

    #5.5 To Do Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import React, { useState } from 'react';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import Router from './Router';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { darkTheme, lightTheme } from './theme';
-import { RecoilRoot, useRecoilValue } from 'recoil';
-import { isDarkAtom } from './atoms';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoList from './TodoList';
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -72,14 +68,10 @@ const GlobalStyle = createGlobalStyle`
   `;
 
 function App() {
-  const isDark = useRecoilValue(isDarkAtom);
   return (
     <>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <Router />
-        <ReactQueryDevtools initialIsOpen={true} />
-      </ThemeProvider>
+      <GlobalStyle />
+      <TodoList />
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
-
-const queryClient = new QueryClient();
+import { darkTheme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </RecoilRoot>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
