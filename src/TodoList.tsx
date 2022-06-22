fe22fbf52596ed9dfc896a74a1e24cabfe131b3f
doyeon <doyeon311@gmail.com>
2022-06-22 23:38:16 +0900

    #5.6 Forms

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,24 +1,48 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+// const TodoList = () => {
+//   const [todo, setTodo] = useState('');
+//   const [todoError, setTodoError] = useState('');
+
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setTodoError('');
+//     setTodo(value);
+//   };
+
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     setTodoError('to do should be longer than 10');
+//     console.log(todo);
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={todo} placeholder='Write a to do' />
+//         <button>Add</button>
+//         {todo.length > 10 ? todoError : null}
+//       </form>
+//     </div>
+//   );
+// };
 
 const TodoList = () => {
-  const [todo, setTodo] = useState('');
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(todo);
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder='Write a to do' />
+      <form>
+        <input {...register('email')} type='text' placeholder='email' />
+        <input {...register('firstName')} type='text' placeholder='firstName' />
+        <input {...register('lastName')} type='text' placeholder='lastName' />
+        <input {...register('username')} type='text' placeholder='username' />
+        <input {...register('password')} type='text' placeholder='password' />
+        <input {...register('passwordConfirm')} type='text' placeholder='passwordConfirm' />
         <button>Add</button>
       </form>
     </div>
