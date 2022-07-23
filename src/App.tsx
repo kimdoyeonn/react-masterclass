9f16cc837d277802676c110706024b404e8728bb
kimdoyeonn <doyeon311@gmail.com>
2022-07-23 17:39:19 +0900

    #7.6 Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -13,26 +14,41 @@ const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
 `;
 
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   tap: { scale: 1, borderRadius: '100px' },
-  drag: { backgroundColor: 'rgb(46, 204, 113)', transition: { duration: 10} },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null)
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileDrag='drag'
-        whileHover='hover'
-        whileTap='tap'
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileDrag='drag'
+          whileHover='hover'
+          whileTap='tap'
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
