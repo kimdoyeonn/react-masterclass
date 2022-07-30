f28304c10c0db62e86edd4c0abf27a86c3f60d65
kimdoyeonn <doyeon311@gmail.com>
2022-07-30 22:52:20 +0900

    #7.12 Slider part One

--- README.md
+++ README.md
@@ -8,3 +8,52 @@
 
 ### framer-motion
 애니메이션을 쉽게 구현할 수 있도록 해주는 라이브러리
+
+
+### #7.12 Slider part One
+
+`AnimatePresence`으로 간단하게 슬라이드 만들기
+
+- variants 객체를 만들고 그 안에 initial, animate, exit에 사용할 애니메이션을 정의해 준 다음 사용할 요소에 적용해줌
+
+```js
+  const boxVariants = {
+    initial: {
+      x: 500,
+      opacity: 0,
+      scale: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 1,
+      }
+    },
+    exit: {
+      opacity: 0,
+      scale: 0,
+      x: -500,
+      transition: {
+        duration: 1,
+      }
+    },
+  };
+
+  <AnimatePresence>
+  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+    i === visible ? (
+      <Box
+        key={i}
+        variants={boxVariants}
+        initial='initial'
+        animate='visible'
+        exit='exit'
+      >
+        {i}
+      </Box>
+    ) : null
+  )}
+</AnimatePresence>
+```
--- src/App.tsx
+++ src/App.tsx
@@ -23,42 +23,62 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
   initial: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
+    transition: {
+      duration: 1,
+    }
   },
-  leaving: {
+  exit: {
     opacity: 0,
     scale: 0,
-    y: 100,
+    x: -500,
+    transition: {
+      duration: 1,
+    }
   },
 };
 
 function App() {
-  const [show, setShow] = useState(false);
-  const toggleShow = () => setShow((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+
   return (
     <Wrapper>
       <AnimatePresence>
-        {show ? (
-          <Box
-            variants={boxVariants}
-            initial='initial'
-            animate='visible'
-            exit='leaving'
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              key={i}
+              variants={boxVariants}
+              initial='initial'
+              animate='visible'
+              exit='exit'
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
-      <button onClick={toggleShow}>click me</button>
+      <button onClick={prevPlease}>prev</button>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
