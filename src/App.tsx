e7fc880efda1aa4b4f60e30b6cd27330b13cd0f3
doyeon <doyeon311@gmail.com>
2022-08-05 23:06:46 +0900

    #7.15 Final Project part One

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,25 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
   background-color: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 500px;
-  height: 500px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 20px;
   display: flex;
@@ -30,20 +41,36 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  border-radius: 50%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
   const [click, setClick] = useState(false);
   return (
     <Wrapper onClick={() => setClick(!click)}>
-      <Box>{!click ? <Circle layoutId="circle" style={{ borderRadius: '50%', scale: 2 }} /> : null}</Box>
-      <Box>{click ? <Circle layoutId="circle" style={{ borderRadius: 0 }} /> : null}</Box>
+      <Grid>
+        <Box layoutId="hihi" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+      {click ? (
+          <Overlay
+            initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+            animate={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}
+            exit={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+          >
+            <Box layoutId="hihi" style={{ width: 300, height: 300 }} />
+          </Overlay>
+      ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
