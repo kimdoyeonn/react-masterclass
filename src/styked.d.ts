3fd4731a0d6e1e4ce03c5dd63ce60207b29306c7
doyeon <doyeon311@gmail.com>
2022-08-08 22:53:32 +0900

    initialize defore next chapter

--- src/styked.d.ts
+++ src/styked.d.ts
@@ -1,9 +1,16 @@
 import "styled-components";
 
 declare module "styled-components" {
-  export interface DefaultTheme {
-    bgColor: string;
-    boardColor: string;
-    cardColor: string;
-  }
+  export const theme: DefaultTheme = {
+    red: "#E51013",
+    black: {
+      veryDark: "#141414",
+      darker: "#181818",
+      lighter: "#2F2F2F",
+    },
+    white: {
+      lighter: "#fff",
+      darker: "#e5e5e5",
+    },
+  };
 }
