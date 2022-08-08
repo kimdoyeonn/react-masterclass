6f82c21e0924197cac18bc758eda5a084662b389
doyeon <doyeon311@gmail.com>
2022-08-08 23:10:03 +0900

    #8.1 Header part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/"/>
+        <Route path="/tv"/>
+        <Route path="/search"/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
