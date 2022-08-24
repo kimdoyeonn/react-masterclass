2ef44e59d5a3e7d9ecb4180fbd9bf13b16d74e2f
doyeon <doyeon311@gmail.com>
2022-08-24 22:57:21 +0900

    #8.13 Movie Modal part Three

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -93,13 +93,38 @@ const Overlay = styled(motion.div)`
 `;
 
 const BigMovie = styled(motion.div)`
-  position: 'absolute';
-  width: '40vw';
-  height: '80vh';
-  background-color: 'red';
+  position: absolute;
+  width: 40vw;
+  height: 80vh;
+  background-color: red;
   left: 0;
   right: 0;
-  margin: '0 auto';
+  margin: 0 auto;
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.black.lighter};
+`;
+
+const BigCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
+`;
+
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
 `;
 
 const rowVariants = {
@@ -168,6 +193,7 @@ function Home() {
   };
 
   const onOverlayClicked = () => navigate('/');
+  const clickedMovie = bigMovieMatch?.params.movieId && data?.results.find(movie => String(movie.id) === bigMovieMatch?.params.movieId);
 
   return (
     <Wrapper>
@@ -226,7 +252,15 @@ function Home() {
                     top: scrollY.get() + 100,
                   }}
                   layoutId={bigMovieMatch.params.movieId}
-                />
+                >
+                  {clickedMovie && (
+                    <>
+                      <BigCover style={{ backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(clickedMovie.backdrop_path, "w500")})` }} />
+                      <BigTitle>{clickedMovie.title}</BigTitle>
+                      <BigOverview>{clickedMovie.overview}</BigOverview>
+                    </>
+                  )}
+                </BigMovie>
               </Overlay>
             ) : null}
           </AnimatePresence>
