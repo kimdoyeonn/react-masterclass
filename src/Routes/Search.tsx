1e7f105084cc23e81705c3175f1dca607af8f958
doyeon <doyeon311@gmail.com>
2022-08-25 23:04:09 +0900

    #8.14 Search Redirect

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,4 +1,9 @@
+import { useLocation, useSearchParams } from "react-router-dom";
+
 function Search() {
+  const [searchParams, _] = useSearchParams();
+  const keyword = searchParams.get('keyword')
+  
   return null;
 }
 
